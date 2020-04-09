1ced41dff3c9c88eb6ba9317e83db8d0f461165f
Omar Desogus <me@cedoor.dev>
2020-04-09 17:37:30 +0200

    [test] parameter types and function visibility

--- test/test.js
+++ test/test.js
@@ -5,14 +5,61 @@ const movigo = require('../dist/movigo')
 ;(async function IIFE () {
   global.window = (await JSDOM.fromFile('index.html')).window
 
-  /** ... **/
+  tape('Target function should throw an exception if the parameter is not a string', function (test) {
+    const wrongTypes = [1, true, () => null, {}, []]
 
-  tape('Core functions', function (test) {
-    const transform = movigo.animate('div')
+    for (const wrongType of wrongTypes) {
+      test.throws(function () {
+        movigo.target(wrongType)
+      })
+    }
 
-    test.equal(typeof transform, 'function')
+    test.end()
+  })
 
-    test.doesNotThrow(transform.bind(this, 'rotateY(60deg)'))
+  tape('Target function should throw an exception if the parameter is not a valid DOM selector', function (test) {
+    test.throws(function () {
+      movigo.target('#wrong')
+    })
+
+    test.end()
+  })
+
+  tape('Target function should return an object of only action functions', function (test) {
+    const target = movigo.target('div')
+
+    test.equal(typeof target, 'object')
+    test.equal(typeof target['translateX'], 'function')
+    test.notEqual(typeof target['animate'], 'function')
+
+    test.end()
+  })
+
+  tape('Action functions should throw an exception if the parameter is not a number', function (test) {
+    const wrongTypes = ['string', true, () => null, {}, []]
+
+    for (const wrongType of wrongTypes) {
+      test.throws(function () {
+        movigo.target('div').translateX(wrongType)
+      })
+    }
+
+    test.end()
+  })
+
+  tape('Action functions should return an object with action and animate functions', function (test) {
+    const target = movigo.target('div').translateX(100)
+
+    test.equal(typeof target['translateX'], 'function')
+    test.equal(typeof target['animate'], 'function')
+
+    test.end()
+  })
+
+  tape('Animate function should return nothing', function (test) {
+    const target = movigo.target('div').translateX(100)
+
+    test.equal(target.animate(), undefined)
 
     test.end()
   })
